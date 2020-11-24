cf10b21205392ad1cd63ebb0ad8b9b8f3507262e
aGiaa91 <36173586+aGiaa91@users.noreply.github.com>
2020-11-24 10:20:26 +0100

    Update table.blade.php

--- table.blade.php
+++ table.blade.php
@@ -14,15 +14,6 @@
             <tr class="myTr">
                 <td class="num">{{$i++}}.</td>
                 <td class="filterable">{{ $one->title }}</td>
-                <td>
-                    {!! Form::open(['route' => ['data.destroy', $one->id], 'method' => 'delete']) !!}
-                    <div class='btn-group'>
-                        <a href="{{ route('data.show', [$one->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                        <a href="{{ route('data.edit', [$one->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                        {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
-                    </div>
-                    {!! Form::close() !!}
-                </td>
             </tr>
         @endforeach
         </tbody>
